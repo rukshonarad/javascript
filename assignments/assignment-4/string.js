32fcc7b8b5a742b4daa22b979d5f31b98de45c2e
Rukshona Rad <rukshona.rad@gmail.com>
2023-06-03 18:02:38 -0400

    Finish assignment -4

---
+++ assignments/assignment-4/string.js
@@ -0,0 +1,24 @@
+// var part1 = "The greatest glory in";
+// var part2 = "living lies not in";
+// var part3 = "never falling, but in";
+// var part4 = "rising every time";
+// var part5 = "we fall.";
+// var author = " - Nelson Mandela";
+// var result = part1.concat(part2, part3, part4, part5, author);
+
+// console.log(`${part1} ${part2} ${part3} ${part4} ${part5} ${author}`);
+//
+//console.log(part1 + part2 + part3 + part4 + part5 + author);
+//
+// console.log(result);
+var part1 = "The greatest glory in";
+part1 += " living lies not in";
+console.log(part1);
+part1 += " never falling, but in";
+console.log(part1);
+part1 += " rising every time";
+console.log(part1);
+part1 += " we fall.";
+console.log(part1);
+part1 += " - Nelson Mandela";
+console.log(part1);
