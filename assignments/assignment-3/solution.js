1f04ac9f5ffc55d4295a0c50f1aa34fac2fcec2a
Rukshona Rad <rukshona.rad@gmail.com>
2023-06-03 18:39:13 -0400

    Finish assignment-3

--- assignments/assignment-3/solution.js
+++ assignments/assignment-3/solution.js
@@ -1,17 +1,50 @@
-var num1 = 6;
-var num2 = 4;
-console.log(num1 + num1);
-console.log(num1 - num2);
-console.log(num1 * num2);
-console.log(num1 / num2);
+// var num1 = 6;
+// var num2 = 4;
+// console.log(num1 + num1);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
 
 // 2-part//
 
-var fahrenheit = 100;
-var celsius = (fahrenheit - 32) * (5 / 9);
-console.log(celsius);
-console.log(fahrenheit);
-var celsius = 200;
-var fahrenheit = celsius * (9 / 5) + 32;
-console.log(fahrenheit);
-console.log(celsius);
+// var fahrenheit = 100;
+// var celsius = (fahrenheit - 32) * (5 / 9);
+// console.log(celsius);
+// console.log(fahrenheit);
+// var celsius = 200;
+// var fahrenheit = celsius * (9 / 5) + 32;
+// console.log(fahrenheit);
+// console.log(celsius);
+
+//Part -3//
+
+// var principal = 50;
+// var rate = 10;
+// var time = 2;
+// interest = (principal * rate * time) / 100;
+// console.log(interest);
+
+//Part-4//
+
+var a = 25;
+var b = 30;
+// a += b;
+// console.log(a);
+// a -= b;
+// console.log(a);
+b -= a;
+console.log(b);
+b += a;
+console.log(b);
+console.log(a);
+
+// Part- 5.1 //
+var radius = 180;
+var area = 3.14 * radius * radius;
+console.log(area);
+
+// Part -5.2//
+var length = 150;
+var width = 478;
+var Perimeter = 2 * (length + width);
+console.log(Perimeter);
