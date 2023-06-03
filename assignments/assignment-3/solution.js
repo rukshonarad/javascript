8f50075d7c1bedaf7b056affb4b06de5224377d9
Rukshona Rad <rukshona.rad@gmail.com>
2023-06-03 17:20:52 -0400

    Finish assignment-3  second part

--- assignments/assignment-3/solution.js
+++ assignments/assignment-3/solution.js
@@ -4,3 +4,14 @@ console.log(num1 + num1);
 console.log(num1 - num2);
 console.log(num1 * num2);
 console.log(num1 / num2);
+
+// 2-part//
+
+var fahrenheit = 100;
+var celsius = (fahrenheit - 32) * (5 / 9);
+console.log(celsius);
+console.log(fahrenheit);
+var celsius = 200;
+var fahrenheit = celsius * (9 / 5) + 32;
+console.log(fahrenheit);
+console.log(celsius);
