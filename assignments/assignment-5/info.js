5fb5bccaefda612c2434bf2f77766cc225479604
Rukshona Rad <rukshona.rad@gmail.com>
2023-06-04 20:43:33 -0400

    Finish assignment-5

---
+++ assignments/assignment-5/info.js
@@ -0,0 +1,16 @@
+var name = "Lilly";
+var age = 27;
+var city = "Samarkand";
+var profession = "software engineer";
+var hobby = "painting";
+var food = "sushi";
+var favoriteColor = "pink";
+var favoriteBook = "Al Bukhari hadis";
+var favoriteSong = "zikr song";
+var favoriteSport = "tennis";
+var dreamDestination = "Mekka";
+var favoriteAnimal = "cat";
+var favoriteSeason = "autumn";
+console.log(
+    `Hello, my name is ${name} and I am ${age} year old. I live in ${city}, where I work as a ${profession}. In my free time, I enjoy ${hobby}. My favorite food is ${food} and my favorite color is ${favoriteColor}. My dream is to one day visit ${dreamDestination}. My favorite season of the year is ${favoriteSeason}, because of the beautiful changing colors of the leaves`
+);
