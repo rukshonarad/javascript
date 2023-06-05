c71bf2432c885cffb9e9e79ae461c22015ac2465
Rukshona Rad <rukshona.rad@gmail.com>
2023-06-05 19:30:22 -0400

    Add some changes

--- assignments/assignment-4/string.js
+++ assignments/assignment-4/string.js
@@ -1,24 +1,36 @@
-// var part1 = "The greatest glory in";
-// var part2 = "living lies not in";
-// var part3 = "never falling, but in";
-// var part4 = "rising every time";
-// var part5 = "we fall.";
-// var author = " - Nelson Mandela";
-// var result = part1.concat(part2, part3, part4, part5, author);
+var part1 = "The greatest glory in";
+var part2 = "living lies not in";
+var part3 = "never falling, but in";
+var part4 = "rising every time";
+var part5 = "we fall.";
+var author = " - Nelson Mandela";
+// var result = part1.concat(
+//     " ",
+//     part2,
+//     " ",
+//     part3,
+//     " ",
+//     part4,
+//     " ",
+//     part5,
+//     author
+// );
 
 // console.log(`${part1} ${part2} ${part3} ${part4} ${part5} ${author}`);
 //
 //console.log(part1 + part2 + part3 + part4 + part5 + author);
 //
-// console.log(result);
-var part1 = "The greatest glory in";
-part1 += " living lies not in";
-console.log(part1);
-part1 += " never falling, but in";
-console.log(part1);
-part1 += " rising every time";
-console.log(part1);
-part1 += " we fall.";
-console.log(part1);
-part1 += " - Nelson Mandela";
-console.log(part1);
+console.log(result);
+
+var result =
+    part1 +
+    " " +
+    part2 +
+    " " +
+    part3 +
+    " " +
+    part4 +
+    " " +
+    part5 +
+    " " +
+    author;
