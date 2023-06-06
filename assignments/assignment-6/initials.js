b5c36ed65ca13f3912df542cb37a7d09abf7d8e0
Rukshona Rad <rukshona.rad@gmail.com>
2023-06-06 04:54:03 -0400

    Finish assignment-6

---
+++ assignments/assignment-6/initials.js
@@ -0,0 +1,40 @@
+//1 //
+var fullName1 = "john doe";
+var fullName2 = "Jane smith";
+var fullName3 = "bob Johnson";
+
+var spaceName1 = fullName1.indexOf(" ");
+
+var firstNameInitial = fullName1[0];
+var lastNameInitial = spaceName1 + 1;
+
+var lastNameInitial = fullName1[lastNameInitial];
+console.log(firstNameInitial);
+console.log(lastNameInitial);
+var result1 = (firstNameInitial + lastNameInitial).toUpperCase();
+console.log(result1);
+
+//  2 //
+
+var spaceName2 = fullName2.indexOf(" ");
+
+var firstNameInitial = fullName2[0];
+var lastNameInitial = spaceName2 + 1;
+
+var lastNameInitial = fullName2[lastNameInitial];
+console.log(firstNameInitial);
+console.log(lastNameInitial);
+var result2 = (firstNameInitial + lastNameInitial).toUpperCase();
+console.log(result2);
+
+//3 //
+var spaceName3 = fullName3.indexOf(" ");
+
+var firstNameInitial = fullName3[0];
+var lastNameInitial = spaceName3 + 1;
+
+var lastNameInitial = fullName3[lastNameInitial];
+console.log(firstNameInitial);
+console.log(lastNameInitial);
+var result3 = (firstNameInitial + lastNameInitial).toUpperCase();
+console.log(result3);
