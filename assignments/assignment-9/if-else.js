654850c8ea6c816f0ba406ff55c7f438702ee3ba
Rukshona Rad <rukshona.rad@gmail.com>
2023-06-07 19:13:54 -0400

    Fix assignment-9

--- assignments/assignment-9/if-else.js
+++ assignments/assignment-9/if-else.js
@@ -2,27 +2,35 @@
 var number1 = Math.floor(Math.random() * 10) + 1;
 
 if (number1 >= 1 && number1 <= 3) {
-    console.log("Bob");
-} else if (number1 <= 4 && number1 <= 7) console.log("Martin");
-else {
-    console.log("Jennifer");
+    name = "Bob";
+} else if (number1 <= 4 && number1 <= 7) {
+    name = "Martin";
+} else {
+    name = "Jennifer";
 }
 //Second Part //
-var vinner = "Rukhshona";
 
-var name = Math.floor(Math.random() * 4 + 1);
-if (name === 1) {
+var number2 = Math.floor(Math.random() * 4 + 1);
+if (number2 === 1) {
     console.log(
-        `Dear ${vinner}, wishing you a day filled with happiness and a year filled with joy`
+        "Dear " +
+            name +
+            ", wishing you a day filled with happiness and a year filled with joy."
     );
-} else if (name === 2) {
+} else if (number2 === 2) {
     console.log(
-        `Dear ${vinner}, sending you smiles for every moment of your special day.`
+        "Dear " +
+            name +
+            ", sending you smiles for every moment of your special day."
     );
-} else if (name === 3) {
-    console.log(`Dear ${vinner}, have a wonderful time and a very happy day!`);
+} else if (number2 === 3) {
+    console.log(
+        "Dear " + name + ", have a wonderful time and a very happy day!"
+    );
 } else {
     console.log(
-        `Dear ${vinner}, hope your special day brings you all that your heart desires!`
+        "Dear " +
+            number2 +
+            ", hope your special day brings you all that your heart desires!"
     );
 }
