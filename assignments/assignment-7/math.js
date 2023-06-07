a418e1f848e4c571f6e70e62319c9943be8d0cce
Rukshona Rad <rukshona.rad@gmail.com>
2023-06-07 06:05:52 -0400

    Finsh assignment-8

--- assignments/assignment-7/math.js
+++ assignments/assignment-7/math.js
@@ -8,23 +8,23 @@ Math.round(3874.4999); // 3875 //
 Math.ceil(789.01); // 790 //
 Math.floor(999.999); // 999 //
 Math.round(-2934.8); // -2935 //
-Math.ceil(-7856.1);
-Math.floor(-8745.9);
-Math.round(0.5534);
-Math.ceil(0.0167);
-Math.floor(0.9999);
-Math.round(-0.5543);
-Math.ceil(-0.1234);
-Math.floor(-0.9876);
-Math.round(9876.5);
-Math.ceil(8765.1);
-Math.floor(7654.9);
-Math.round(-6543.5);
-Math.ceil(-5432.1);
-Math.floor(-4321.9);
-Math.round(0);
-Math.ceil(0);
-Math.floor(0);
-Math.round(10000);
-Math.ceil(10000);
-Math.floor(10000);
+Math.ceil(-7856.1); // -7857 //
+Math.floor(-8745.9); // -8746 //
+Math.round(0.5534); // 1 //
+Math.ceil(0.0167); // 1 //
+Math.floor(0.9999); // 0 //
+Math.round(-0.5543); // 1 //
+Math.ceil(-0.1234); // 0 //
+Math.floor(-0.9876); // -1 //
+Math.round(9876.5); // 9876//
+Math.ceil(8765.1); // 8766 //
+Math.floor(7654.9); // 7654 //
+Math.round(-6543.5); // -6544 //
+Math.ceil(-5432.1); // -5432 //
+Math.floor(-4321.9); // -4322 //
+Math.round(0); // 0 //
+Math.ceil(0); // 0 //
+Math.floor(0); // 0 //
+Math.round(10000); // 10000 //
+Math.ceil(10000); // 10000 //
+Math.floor(10000); // 10000 //
---
+++ assignments/assignment-8/math.js
@@ -0,0 +1,30 @@
+// 1-10 //
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.ceil(Math.random() * 10) + 1);
+// 1-100 //
+console.log(Math.floor(Math.random() * 100) + 1);
+console.log(Math.ceil(Math.random() * 100) + 1);
+// 50 -100 //
+console.log(Math.floor(Math.random() * 50) + 51);
+console.log(Math.ceil(Math.random() * 50) + 51);
+//-10 - 10 //
+console.log(Math.floor(Math.random() * 20) - 10);
+console.log(Math.ceil(Math.random() * 20) - 10);
+// -100 - 100//
+console.log(Math.floor(Math.random() * 200) - 100);
+console.log(Math.ceil(Math.random() * 200) - 100);
+// 1000 - 2000 //
+console.log(Math.floor(Math.random() * 1000) + 1001);
+console.log(Math.ceil(Math.random() * 1000) + 1001);
+// -1000 - 0 //
+console.log(Math.floor(Math.random() * 1000) - 1001);
+console.log(Math.ceil(Math.random() * 1000) - 1001);
+// 1 - 1000 //
+console.log(Math.floor(Math.random() * 1000) + 1);
+console.log(Math.ceil(Math.random() * 1000) + 1);
+//-500 - 500 //
+console.log(Math.floor(Math.random() * 1000) - 500);
+console.log(Math.ceil(Math.random() * 1000) - 500);
+//10000 - 20000 //
+console.log(Math.floor(Math.random() * 10000) + 10001);
+console.log(Math.ceil(Math.random() * 10000) + 10001);
