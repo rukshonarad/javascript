39bb6cadbc659b051b4c7126d677c8c9e967551c
Rukshona Rad <rukshona.rad@gmail.com>
2023-06-12 22:18:54 -0400

    Add some changes to assignment-13

--- assignments/assignment-13/array.js
+++ assignments/assignment-13/array.js
@@ -5,61 +5,65 @@ function combineArrays(arr1, arr2, arr3) {
 }
 console.log(combineArrays([1, 2, 3], [4, 5, 6]));
 
-2;
-function reversOrder(arr) {
-    var result = arr.toReversed();
-    return result;
+//2;
+function reverseOrder(arr) {
+    arr.reverse(arr);
+    return arr;
 }
+console.log(reverseOrder([1, 2, 3, 4, 5]));
 
-// console.log(reversOrder([1, 2, 3, 4, 5]));
-// 3
+//3;
 function checkValue(arr, val) {
     if (arr.includes(val)) {
         return true;
-    } else false;
+    } else {
+        false;
+    }
 }
 console.log(checkValue([56, 766, 34], 34));
-4;
+// //4
+
 function isArray(arr) {
     if (Array.isArray(arr)) {
         return true;
-    } else false;
+    } else {
+        return false;
+    }
 }
 console.log(isArray([10, 25, 35, 44]));
-// 5
+
+// // 5
 function arraySlice(arr, start, end) {
     return arr.slice(start, end);
 }
 console.log(arraySlice([1, 2, 3, 4, 5, 6, 7], 1, 4));
-//6
+
+// // //6
 function removeLast(arr) {
-    var result = arr.pop();
-    var result = arr;
-    return result;
+    arr.pop();
+    return arr;
 }
 console.log(removeLast(["Sun", "Mon", "Tue", "Wed"]));
 
 // // 7
 function removFirst(arr) {
-    var result = arr.shift();
-    var result = arr;
-    return result;
+    arr.shift();
+    return arr;
 }
 console.log(removFirst(["book", "note", "pencil"]));
-//8
+
+// //8
 function addAtBeggining(arr, num) {
-    var result = arr.unshift(num);
-    var result = arr;
-    return result;
+    arr.unshift(num);
+    return arr;
 }
 console.log(addAtBeggining([2, 3, 4, 5], 1));
 
-// 9
+// // 9
 
 function addAtEnd(arr, num) {
-    var result = arr.push(num);
-    var result = arr;
-    return result;
+    arr.push(num);
+    return arr;
 }
 console.log(
     addAtEnd(["January", "February", "March", "April", "May", "June"], "July")
