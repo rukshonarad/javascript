8b0b4b28989f5d56eb5d9936dd6734f42a1d51c4
Rukshona Rad <rukshona.rad@gmail.com>
2023-08-05 06:04:51 -0400

    Finish assessment-11

--- assignments/assignment-27/script.js
+++ assignments/assignment-27/script.js
@@ -1,131 +1,131 @@
-// // 1. Find the Maximum Number
-// function findMaxNumber(arr) {
-//     let maxNum = arr[0][0];
-//     for (let i = 0; i < arr.length; i++) {
-//         for (let j = 0; j < arr[i].length; j++) {
-//             if (arr[i][j] > maxNum) {
-//                 maxNum = arr[i][j];
-//             }
-//         }
-//     }
-//     return maxNum;
-// }
-// let input = [
-//     [1, 2, 3],
-//     [4, 5, 6],
-//     [7, 8, 9]
-// ];
-// let maxNumber = findMaxNumber(input);
-// console.log("Maximum number:", maxNumber);
-// // Output: Maximum number: 9
+// 1. Find the Maximum Number
+function findMaxNumber(arr) {
+    let maxNum = arr[0][0];
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            if (arr[i][j] > maxNum) {
+                maxNum = arr[i][j];
+            }
+        }
+    }
+    return maxNum;
+}
+let input = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+let maxNumber = findMaxNumber(input);
+console.log("Maximum number:", maxNumber);
+// Output: Maximum number: 9
 
-// //  2.  Count Characters//
-// function countCharacters(arr) {
-//     let charCout = {};
-//     for (let i = 0; i < arr.length; i++) {
-//         for (let j = 0; j < arr[i].length; j++) {
-//             let char = arr[i][j];
-//             if (charCout[char]) {
-//                 charCout[char]++;
-//             } else {
-//                 charCout[char] = 1;
-//             }
-//         }
-//     }
-//     return charCout;
-// }
-// console.log(
-//     countCharacters([
-//         ["a", "b", "a"],
-//         ["c", "a", "d"]
-//     ])
-// );
+//  2.  Count Characters//
+function countCharacters(arr) {
+    let charCout = {};
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            let char = arr[i][j];
+            if (charCout[char]) {
+                charCout[char]++;
+            } else {
+                charCout[char] = 1;
+            }
+        }
+    }
+    return charCout;
+}
+console.log(
+    countCharacters([
+        ["a", "b", "a"],
+        ["c", "a", "d"]
+    ])
+);
 
-// // Output: { a: 3, b: 1, c: 1, d: 1 }
+// Output: { a: 3, b: 1, c: 1, d: 1 }
 
-// // 3. Filter Divisible Numbers//
-// function filterDivisibleByThree(arr) {
-//     let newArr = [];
-//     for (let i = 0; i < arr.length; i++) {
-//         for (let j = 0; j < arr[i].length; j++) {
-//             let char = arr[i][j];
-//             if (char % 3 === 0) {
-//                 newArr.push(char);
-//             }
-//         }
-//     }
-//     return newArr;
-// }
-// console.log(
-//     filterDivisibleByThree([
-//         [1, 2, 3],
-//         [4, 5, 6],
-//         [7, 8, 9]
-//     ])
-// );
+// 3. Filter Divisible Numbers//
+function filterDivisibleByThree(arr) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            let char = arr[i][j];
+            if (char % 3 === 0) {
+                newArr.push(char);
+            }
+        }
+    }
+    return newArr;
+}
+console.log(
+    filterDivisibleByThree([
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ])
+);
 
-// //Output: [3, 6, 9]
+//Output: [3, 6, 9]
 
-// // 4. Find the Longest String
-// function findLongestString(arr) {
-//     let longestStr = "";
-//     for (let i = 0; i < arr.length; i++) {
-//         for (let j = 0; j < arr[i].length; j++) {
-//             let char = arr[i][j];
-//             if (char.length > longestStr.length) {
-//                 longestStr = char;
-//             }
-//         }
-//     }
-//     return longestStr;
-// }
-// console.log(
-//     findLongestString([
-//         ["apple", "banana"],
-//         ["carrot", "date", "eggplant"],
-//         ["fig"]
-//     ])
-// );
-// // Output: egplant
+// 4. Find the Longest String
+function findLongestString(arr) {
+    let longestStr = "";
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            let char = arr[i][j];
+            if (char.length > longestStr.length) {
+                longestStr = char;
+            }
+        }
+    }
+    return longestStr;
+}
+console.log(
+    findLongestString([
+        ["apple", "banana"],
+        ["carrot", "date", "eggplant"],
+        ["fig"]
+    ])
+);
+// Output: egplant
 
-// //5. Sum of Numbers (including nested arrays)
-// function sumArray(arr) {
-//     let sum = 0;
+//5. Sum of Numbers (including nested arrays)
+function sumArray(arr) {
+    let sum = 0;
 
-//     for (let i = 0; i < arr.length; i++) {
-//         if (Array.isArray(arr[i])) {
-//             for (let j = 0; j < arr[i].length; j++) {
-//                 sum += arr[i][j];
-//             }
-//         } else {
-//             sum += arr[i];
-//         }
-//     }
+    for (let i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i])) {
+            for (let j = 0; j < arr[i].length; j++) {
+                sum += arr[i][j];
+            }
+        } else {
+            sum += arr[i];
+        }
+    }
 
-//     return sum;
-// }
+    return sum;
+}
 
-// console.log(sumArray([1, 2, [3, 4, 5, 6], 7, [8, 9]]));
+console.log(sumArray([1, 2, [3, 4, 5, 6], 7, [8, 9]]));
 
-// //Output: 45;
+//Output: 45;
 
-// //6. Capitalize Strings
-// function capitalizeArr(arr) {
-//     let newArr = [];
-//     for (let i = 0; i < arr.length; i++) {
-//         let capitalizeSubArr = [];
-//         for (let j = 0; j < arr[i].length; j++) {
-//             let char = arr[i][j].toUpperCase();
-//             capitalizeSubArr.push(char);
-//         }
-//         newArr.push(capitalizeSubArr);
-//     }
-//     return newArr;
-// }
-// let arr = [["apple", "banana"], ["carrot", "date"], ["eggplant"]];
-// console.log(capitalizeArr(arr));
+//6. Capitalize Strings
+function capitalizeArr(arr) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        let capitalizeSubArr = [];
+        for (let j = 0; j < arr[i].length; j++) {
+            let char = arr[i][j].toUpperCase();
+            capitalizeSubArr.push(char);
+        }
+        newArr.push(capitalizeSubArr);
+    }
+    return newArr;
+}
+let arr = [["apple", "banana"], ["carrot", "date"], ["eggplant"]];
+console.log(capitalizeArr(arr));
 
-//Output: [['APPLE', 'BANANA'], ['CARROT', 'DATE'], ['EGGPLANT']]
+Output: [["APPLE", "BANANA"], ["CARROT", "DATE"], ["EGGPLANT"]];
 
 // 7. Find Minimum and Maximum Arrays
 function findMinMaxArrays(arr) {
